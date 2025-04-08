f59457c8536ecd02732e612f142a799383f954d6
amirhbay <amirbay1375@gmail.com>
2025-04-09 03:02:27 +0330

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,33 @@
+const CACHE_NAME = 'gold-bubble-v1';
+const urlsToCache = [
+  '/',
+  '/index.html', // مطمئن شوید که نام فایل HTML شما همین است
+  // '/css/style.css', // اگر فایل CSS جداگانه دارید، مسیر آن را اینجا اضافه کنید
+  // '/js/app.js',   // اگر فایل JS جداگانه دارید، مسیر آن را اینجا اضافه کنید
+  '/icons/icon-192x192.png',
+  '/icons/icon-512x512.png'
+];
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
